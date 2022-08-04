e89a2130e5d853c40637ee7c6bb7acb0f84dae0a
Stuart Groom <stuartgroom98@gmail.com>
2022-08-04 21:23:57 +0100

    product page started

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ for (i = 0; i < faq.length; i++) {
     });
 }
 
+//product 
+document.querySelectorAll( '.productCircle' ).forEach( ( productCircle )=>{
+    let circles = productCircle.querySelectorAll( '.circle' )
+    let angle = 360-90, dangle = 360 / circles.length
+    for( let i = 0; i < circles.length; ++i ){
+      let circle = circles[i]
+      angle += dangle
+      circle.style.transform = `rotate(${angle}deg) translate(${productCircle.clientWidth / 2}px) rotate(-${angle}deg)`
+    }
+})
