f7d74eb1b5d4ef52e4c750cff4cab3fd7c66157b
Stuart Groom <stuartgroom98@gmail.com>
2022-08-05 23:58:33 +0100

    added to about us page, reduced nav margins

--- product.js
+++ product.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
 
     $('.colourChoose input').on('click', function() {
         var productColour = $(this).attr('data-image');
-
+        
         $('.active').removeClass('active');
         $('.leftColumn img[data-image = ' + productColour + ']').addClass('active');
         $(this).addClass('active');
     });
 
 });
+
+
+
