d2b033b0d940d38554507e09632c4f929ce2d0b2
Stuart Groom <stuartgroom98@gmail.com>
2022-08-08 21:14:35 +0100

    hover on product icon to show text

--- product.js
+++ product.js
@@ -11,4 +11,30 @@ $(document).ready(function() {
 });
 
 
+function show() {
+ 
+    /* Access image by id and change
+    the display property to block*/
+    // document.getElementById('discrete').style.display = "block";
+
+    document.getElementById('discreteButton').style.background = "rgb(255,240,245)";
+    // document.getElementById('discreteButton').style.border = "black";
+    document.getElementById('hoverText').style.visibility = "hidden"; 
+
+    document.getElementById('hiddenText').style.visibility = "inherit"; 
+}
+
+function hide(){
+    /* Access image by id and change
+    the display property to block*/
+    document.getElementById('discrete').style.display = "none";
+
+    document.getElementById('discreteButton').style.background = "rgba(224,255,251,255)";
+    document.getElementById('hoverText').style.visibility = "inherit"; 
+
+    document.getElementById('hiddenText').style.visibility = "hidden"; 
+}
+
+
+
 
