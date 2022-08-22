e33e86970d1eda5986a216a7d9204660e34dcd0a
Stuart Groom <stuartgroom98@gmail.com>
2022-08-22 23:04:34 +0100

    icons and corresponding info working

--- product.js
+++ product.js
@@ -19,80 +19,36 @@ function hideAll(){
     document.getElementById('hiddenText6').style.display = "none";
 }
 
-function show() {
-    /* Access image by id and change
-    the display property to block*/
+function show() {; 
     document.getElementById('discreteButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText').style.display = "inherit"; 
-
-    document.getElementById('discreteButton').style.borderWidth = "1px";
-    document.getElementById('infoHolder').style.background = "pink";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "blue";
+    document.getElementById("cardProductImage").style.marginLeft = "200px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "visible";
+    document.getElementById("cardContent").style.opacity = "1";
+    document.querySelector(".content .discreteInfo").style.visibility = "visible";
+    document.querySelector(".content .discreteInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
 
 }
 
 function hide(){
-    /* Access image by id and change
-    the display property to block*/
     document.getElementById('discreteButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText').style.display = "none"; 
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
+    document.getElementById("cardProductImage").style.marginLeft = "0px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "hidden";
+    document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .discreteInfo").style.visibility = "hidden";
+    document.querySelector(".content .discreteInfo").style.display = "none";
 }
 
 function show2() {  
     document.getElementById('attachButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText2').style.display = "inherit"; 
-
-    document.getElementById('infoHolder').style.background = "pink";
-}
-
-function hide2(){
-    document.getElementById('attachButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText2').style.display = "none"; 
-}
-
-function show3() {
-    document.getElementById('batteryButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText3').style.display = "inherit"; 
-}
-
-function hide3(){
-    document.getElementById('batteryButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText3').style.display = "none"; 
-}
-
-function show4() {
-    document.getElementById('buildButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText4').style.display = "inherit"; 
-}
-
-function hide4(){
-    document.getElementById('buildButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText4').style.display = "none"; 
-}
-
-function show5() {
-    document.getElementById('useButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText5').style.display = "inherit"; 
-}
-
-function hide5(){
-    document.getElementById('useButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText5').style.display = "none"; 
-}
-
-function show6() {
-    document.getElementById('alarmButton').style.background = "pink";
-    document.getElementById('hoverText').style.display = "none"; 
-    document.getElementById('hiddenText6').style.display = "inherit"; 
 
     /*handles the card transformation*/
     document.getElementById("cardCircle").style.backgroundColor = "blue";
@@ -100,25 +56,138 @@ function show6() {
     document.getElementById("cardContent").style.left = "0";
     document.getElementById("cardContent").style.visibility = "visible";
     document.getElementById("cardContent").style.opacity = "1";
-
-    document.getElementById("alarmText").style.visibility = "visible";
-    document.getElementById("discreteText").style.display = "none";
-    document.getElementById("alarmTitle").style.visibility = "visible";
-    document.getElementById("discreteTitle").style.display = "none";
-    /*need to optimise this so it doesnt need to set all to being not visible- check style?*/ 
+    document.querySelector(".content .attachInfo").style.visibility = "visible";
+    document.querySelector(".content .attachInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
 }
 
-function hide6(){
-    document.getElementById('alarmButton').style.background = "white";
-    document.getElementById('hoverText').style.display = "inherit"; 
-    document.getElementById('hiddenText6').style.display = "none"; 
 
+function hide2(){
+    document.getElementById('attachButton').style.background = "white";
     /*handles the card transformation*/
-    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
     document.getElementById("cardProductImage").style.marginLeft = "0px";
     document.getElementById("cardContent").style.left = "0";
     document.getElementById("cardContent").style.visibility = "hidden";
     document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .attachInfo").style.visibility = "hidden";
+    document.querySelector(".content .attachInfo").style.display = "none";
+}
+
+function show3() {
+    document.getElementById('batteryButton').style.background = "pink";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "blue";
+    document.getElementById("cardProductImage").style.marginLeft = "200px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "visible";
+    document.getElementById("cardContent").style.opacity = "1";
+    document.querySelector(".content .batteryInfo").style.visibility = "visible";
+    document.querySelector(".content .batteryInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
+
+
+}
+
+function hide3(){
+    document.getElementById('batteryButton').style.background = "white";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
+    document.getElementById("cardProductImage").style.marginLeft = "0px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "hidden";
+    document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .batteryInfo").style.visibility = "hidden";
+    document.querySelector(".content .batteryInfo").style.display = "none";
+
+}
+
+function show4() {
+    document.getElementById('buildButton').style.background = "pink";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "blue";
+    document.getElementById("cardProductImage").style.marginLeft = "200px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "visible";
+    document.getElementById("cardContent").style.opacity = "1";
+    document.querySelector(".content .buildInfo").style.visibility = "visible";
+    document.querySelector(".content .buildInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
+}
+
+function hide4(){
+    document.getElementById('buildButton').style.background = "white";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
+    document.getElementById("cardProductImage").style.marginLeft = "0px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "hidden";
+    document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .buildInfo").style.visibility = "hidden";
+    document.querySelector(".content .buildInfo").style.display = "none";
+
+}
+
+function show5() {
+    document.getElementById('useButton').style.background = "pink";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "blue";
+    document.getElementById("cardProductImage").style.marginLeft = "200px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "visible";
+    document.getElementById("cardContent").style.opacity = "1";
+    document.querySelector(".content .useInfo").style.visibility = "visible";
+    document.querySelector(".content .useInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
+}
+
+function hide5(){
+    document.getElementById('useButton').style.background = "white";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
+    document.getElementById("cardProductImage").style.marginLeft = "0px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "hidden";
+    document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .useInfo").style.visibility = "hidden";
+    document.querySelector(".content .useInfo").style.display = "none";
+
+}
+
+function show6() {
+    document.getElementById('alarmButton').style.background = "pink";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "blue";
+    document.getElementById("cardProductImage").style.marginLeft = "200px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "visible";
+    document.getElementById("cardContent").style.opacity = "1";
+    document.querySelector(".content .alarmInfo").style.visibility = "visible";
+    document.querySelector(".content .alarmInfo").style.display = "inline-block";
+    document.getElementById("cardCircle").style.backgroundColor = "pink";
+    document.getElementById("cardCircle").style.transition= "0.5s ease";
+    document.getElementById("cardCircle").style.clipPath("circle(400px at center)")
+
+}
+
+function hide6(){
+    document.getElementById('alarmButton').style.background = "white";
+    /*handles the card transformation*/
+    document.getElementById("cardCircle").style.backgroundColor = "#ECF7F8";
+    document.getElementById("cardProductImage").style.marginLeft = "0px";
+    document.getElementById("cardContent").style.left = "0";
+    document.getElementById("cardContent").style.visibility = "hidden";
+    document.getElementById("cardContent").style.opacity = "0";
+    document.querySelector(".content .alarmInfo").style.visibility = "hidden";
+    document.querySelector(".content .alarmInfo").style.display = "none";
 }
 
 
